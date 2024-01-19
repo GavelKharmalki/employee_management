bcf41ce7675ffec8716c77866487f60ce1e2ddc7
Gavel <gavelkharmalki@gmail.com>
2024-01-19 16:43:25 +0530

    Network image

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -44,8 +44,17 @@ class _MainScreenState extends State<MainScreen> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Icon(Icons.star_rate_rounded,color: Colors.deepPurple,size: 50.sp,),
-                          Icon(Icons.person_2_rounded,color: Colors.white,size: 50.sp,),
+                          Icon(Icons.stream_rounded,color: Colors.deepPurple,size: 50.sp,),
+                          
+                          ClipOval(
+      child: Image.network(
+        
+        "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg",
+        width: 30.w,
+        height: 30.h,
+        fit: BoxFit.cover, // Adjust the fit as needed
+      ),
+    ),
                           //Text('ABCD',style: TextStyle(color: Colors.white,fontSize: 24.sp),),
                         ],
                       ),
